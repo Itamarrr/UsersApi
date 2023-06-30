417dea28c8c4c26d3ee14b50f007eb38832f55b2
itamarrr <itamar.oliveira@unigranrio.br>
2023-06-30 19:30:01 -0300

    incluido na program o AddJwtBearer e UseAuthentication

--- UsersAPI.Services/Program.cs
+++ UsersAPI.Services/Program.cs
@@ -3,11 +3,15 @@ using UsersAPI.Services.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 
+
 builder.Services.AddControllers();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);//colocar a rota para minusculo
 //builder.Services.AddEndpointsApiExplorer(); RETIRADO POQUE EU CRIEI UMA CLASSE  PARA ISSO
 //builder.Services.AddSwaggerGen();RETIRADO POQUE EU CRIEI UMA CLASSE PARA ISSO
 builder.Services.AddSwaggerDoc();
+builder.Services.AddJwtBearer();//INCLUINDO A CONFIGURAÇÃO DO JWT
+
+
 
 var app = builder.Build();
 
@@ -17,6 +21,8 @@ var app = builder.Build();
 //    app.UseSwaggerUI();
 //}
 app.UseSwaggerDoc();// roda o Swagger tanto em produção quanto em dev
+
+app.UseAuthentication();// essa é a sequancia primeiro vem esse depois vem o UseAuthorization
 app.UseAuthorization();
 
 app.MapControllers();
