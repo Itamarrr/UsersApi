d90fce1351000c78ab2a0a0272589b4aaf29a574
itamarrr <itamar.oliveira@unigranrio.br>
2023-06-30 19:54:34 -0300

    configurado o CorsExtension

--- UsersAPI.Services/Extensions/CorsExtension.cs
+++ UsersAPI.Services/Extensions/CorsExtension.cs
@@ -1,7 +1,31 @@
 ﻿namespace UsersAPI.Services.Extensions
 {
-    //
-    public class CorsExtension
+    public static class CorsExtension
     {
+        private static string _policyName = "DefaultPolicy";
+
+        public static IServiceCollection AddCors(this IServiceCollection services)
+        {
+            services.AddCors(s => s.AddPolicy(_policyName, builder =>
+            {
+                //qualquer domínio pode acessar a API
+                builder.AllowAnyOrigin()
+                       //qualquer método (POST, PUT, DELETE, GET etc) pode ser acessado
+                       .AllowAnyMethod()
+                       //qualquer parâmetro de cabeçalho de requisição pode ser enviado
+                       .AllowAnyHeader();
+            }));
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseCors(this IApplicationBuilder app)
+        {
+            app.UseCors(_policyName);
+            return app;
+        }
     }
 }
+
+
+
