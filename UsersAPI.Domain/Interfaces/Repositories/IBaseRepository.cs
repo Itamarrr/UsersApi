b2508cb956ed6355fcd21fc79b8fbc735b499237
itamarrr <itamar.oliveira@unigranrio.br>
2023-06-30 21:02:28 -0300

    Criação da interface especifica para implementação especificação do usuario

---
+++ UsersAPI.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersAPI.Domain.Interfaces.Repositories
+{
+    //IDisposable joga fora um recurso que minha classe inicializa e depois de utilizado
+    public interface IBaseRepository<TEntity, TKey> : IDisposable
+        where TEntity : class
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+
+        #region //framewoerke é o entityFraworke para filtrar 
+        List<TEntity> GetAll();
+        List<TEntity> GetAll(Func<TEntity, bool> where);
+        TEntity? Get(TKey id);
+
+        TEntity? Get(Func<TEntity, bool> where);
+        #endregion
+
+
+    }
+}
