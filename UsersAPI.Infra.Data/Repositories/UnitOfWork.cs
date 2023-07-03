184b4755de590b1630097b010994eaefd3bae46f
itamarrr <itamar.oliveira@unigranrio.br>
2023-07-03 10:01:13 -0300

    Criado as classes do Repositories para o UsersAPI.Infra.Data BaseRepository, UnitOfWork e UserRepository

---
+++ UsersAPI.Infra.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersAPI.Domain.Interfaces.Repositories;
+
+namespace UsersAPI.Infra.Data.Repositories
+{
+    //UnitOfWork 
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IUserRepository UserRepository => throw new NotImplementedException();
+        
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
